79ee5d6b99412f4a7b8a69b951a672cf2ca5c078
Akibul Hasan Hridoy <akibulhasanhridoywp@gmail.com>
2025-09-09 23:23:48 +0600

    in First Class with shradha kapura

---
+++ firstTest.py
@@ -0,0 +1,10 @@
+name = "Akibul Hasan Hridoy"
+age = 22
+insNAme = "DPI"
+year = 2024
+print("Hi My Name is", name)
+print("I am a student of ",insNAme)
+print(type(name))
+print(type(age))
+print(type(year))
+print(type(insNAme))
---
+++ firstTestPractice.py
@@ -0,0 +1,22 @@
+# WAP to input side of a square & print its area.
+# height=int( input("Enter a Height of an Squre:"))
+# weight=int(input("Enter a Weight of an Squre:"))
+# area=height*weight
+# print(area)
+
+# WAP to input 2 floating point numbers & print their average.
+
+# a = float(input("Enter a float number:"))
+# b = float(input("Enter a float number:"))
+# avr= (a+b)/2
+# print(avr)
+
+
+# WAP to input 2 int numbers, a and b. 
+# Print True if a is greater than or equal to b. If not print False
+
+a = float(input("Enter a float number:"))
+b = float(input("Enter a float number:"))
+
+ans = a>=b
+print(ans)
